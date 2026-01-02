6fc73ff4c5985f2f51b80b63ff5c6ce71ec6a28f
piyushxyz1 <piyushnegi033@gmail.com>
2026-01-02 21:38:13 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import { my_env_var_val } from './config/myEnv.js';
 import cors from "cors";
 import { connectDB } from './config/db.js';
 import { clerkMiddleware } from '@clerk/express';
+import {serve} from 'inngest/express'
+import { functions,inngest } from './config/inngest.js';
 
 
 const app = express();
@@ -14,7 +16,7 @@ const __dirname = path.resolve();
 app.get("/link",(req,res)=>{
 res.status(200).json({message:"success"})
 });
-
+app.use("/api/inngest",serve({client:inngest,functions}))
 //make our app ready for deployment
 
 if(my_env_var_val.NODE_ENV === "production"){
