bdd00b3043688e380927230070a11522b88f7004
piyushxyz1 <piyushnegi033@gmail.com>
2026-01-02 22:54:57 +0530

    auth-setup-updated

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -20,7 +20,7 @@ const syncUser = inngest.createFunction(
             wishlist: [],
         };
 
-        await User.create(newUser);
+        await Users.create(newUser);
     }
 );
 
@@ -31,7 +31,7 @@ const deleteUserFromDB = inngest.createFunction(
         await connectDB();
 
         const { id } = event.data;
-        await User.deleteOne({ clerkId: id });
+        await Users.deleteOne({ clerkId: id });
     }
 );
 
